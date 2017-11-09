b645cddd3bd4315f5feed2277c6ed636316aa81f
Tiffany Toh Qing Wei <tiffanytoh2005@hotmail.com>
2017-11-09 16:41:15 +0800

    Edit translate.js

--- Translate/translate.js
+++ Translate/translate.js
@@ -5,11 +5,29 @@ var selection = require('selection');
 var menuItem = contextMenu.Item({
     label: "Translate Selection",
     context: contextMenu.SelectionContext(),
-    contextScript: 'self.on("click", function () {' +
+    contentScript: 'self.on("click", function () {' +
                         'var text = window.getSelection().toString();' +
                         'self.postMessage(text);' +
                     '});',
-    
+    onMessage: function (text) {
+        if(text.length === 0) {
+            throw ('Text to translate must not be empty');
+        }
+
+        var req = request.Request({
+            url: "http://ajax.googleapis.com/ajax/services/language/translate",
+            content: {
+                v: "1.0",
+                q: text,
+                langpair: "len"
+            },
+            onComplete: function (response) {
+                translated = response.json.responseData.translatedText;
+                selection.text = translated;
+            }
+        });
+
+        req.get();
     }
 
 })
