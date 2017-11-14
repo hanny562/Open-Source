e529b2b8d71370d281032d76c723158f8460254c
Tiffany Toh Qing Wei <tiffanytoh2005@hotmail.com>
2017-11-14 16:52:23 +0800

    update calculator.js; add sum function

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -13,5 +13,49 @@ function calculate(select) {
         power: '^'
     };
 
-   
+    symbol.ooo = [[[symbol.sum], [symbol.minus], [symbol.div], [symbol.mul]], [[symbol.mod], [symbol.power]]];
+
+    select = select.replace(/[^0-9%^*\/()\-+.]/g, '');
+
+    var output;
+    for (var i = 0, n = symbol.ooo.length; i < n; i++) {
+
+        var re = new RegExp('(\\d+\\.?\\d*)([\\' + symbol.ooo[i].join('\\') + '])(\\d+\\.?\\d*)');
+        re.lastIndex = 0;
+
+        while (re.test(select)) {
+            output = calc_internal(RegExp.$1, RegExp.$2, RegExp.$3);
+            if (isNaN(output) || !isFinite(output)) return output;
+            select = select.replace(re, output);
+        }
+    }
+
+    return output;
+
+    function calc_internal(a, op, b) {
+        a = a * 1; b = b * 1;
+
+        if (op == symbol.sum) {
+            return a + b;
+        }
+        else if (op == symbol.minus) {
+            return a - b;
+        }
+        else if (op == symbol.div) {
+            return a / b;
+        }
+        else if (op == symbol.mul) {
+            return a * b;
+        }
+        else if (op == symbol.mod) {
+            return a % b
+        }
+        else if (op == symbol.power) {
+            return Math.pow(a, b);
+        }
+        else {
+            return null;
+        }
+    }
 }
+
