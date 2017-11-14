e7a21700d99b91b23c36e22bb699d95358dc4e76
Tiffany Toh Qing Wei <tiffanytoh2005@hotmail.com>
2017-11-14 16:42:45 +0800

    added calculator.js

---
+++ Calculator/calculator.js
@@ -0,0 +1,17 @@
+var keys = document.querySelectorAll('#calculator span');
+var operators = ['+', '-', 'x', '/', '%', 'Xy'];
+var decimalAdded = false;
+
+function calculate(select) {
+
+    var symbol = {
+        sum: '+',
+        minus: '-',
+        div: '/',
+        mul: '*',
+        mod: '%',
+        power: '^'
+    };
+
+   
+}
