b8cb96733dc3f6ed09377bc9e54957ce45612df2
hanny562 <hanny562@hotmail.com>
2017-11-14 16:59:16 +0800

    add operator key function; partially

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -59,3 +59,76 @@ function calculate(select) {
     }
 }
 
+for (var i = 0; i < keys.length; i++) {
+    keys[i].onclick = function (e) {
+        var input = document.querySelector('.screen');
+        var inputVal = input.textContent;
+        var button = this.textContent;
+
+        if (button == 'copy') {
+            var copy = function (nmbr, mimetype) {
+                document.oncopy = function (event) {
+                    event.clipboardData.setData(mimetype, nmbr);
+                    event.preventDefault();
+                };
+                document.execCommand('Copy', false, null);
+            }
+            copy(inputVal, 'text/plain');
+            button = '';
+        }
+        if (button == 'c') {
+            input.textContent = '';
+            decimalAdded = false;
+        }
+
+        else if (button == '=') {
+            var equation = inputVal;
+            var lastChar = equation[equation.length - 1];
+
+            equation = equation.replace(/x/g, '*');
+
+            if (lastChar == '%') {
+                var operate = new RegExp("[-+*x]");
+                equation = equation.replace(/%/g, '/100');
+                var prefix = '';
+                for (var f = 0; f < inputVal.length; f++) {
+                    if (prefix.match(operate)) {
+                        break;
+                    } else if (inputVal.charAt(f) == '%') {
+                        prefix = '';
+                        break;
+                    }
+                    prefix += inputVal.charAt(f);
+                }
+                equation = equation + '*' + prefix;
+            } else if (lastChar != '%' && inputVal.indexOf('%') > -1) {
+                equation = equation.replace(/%/, '/100*');
+            }
+
+            if (operators.indexOf(lastChar) > -1 || lastChar == '.' || lastChar == '^')
+                equation = equation.replace(/(^)|.$/, '');
+
+            if (equation)
+                input.textContent = calculate(equation);
+
+            if (input.textContent.length > 14) {
+                var n;
+                var divs = document.getElementsByClassName('screen');
+                for (n = 0; n < divs.length; n++) {
+                    if (divs[n].className == 'screen') {
+                        divs[n].textContent = divs[n].textContent.substring(0, 15);
+                    }
+                }
+
+                if (!decimalAdded) {
+                    input.textContent = calculate(equation).toPrecision(14);
+                }
+            }
+
+            decimalAdded = false;
+        }
+
+    }
+
+}
+
