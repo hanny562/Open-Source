c123d622652e81574637af52fcfe975996a4bf53
Tiffany Toh Qing Wei <tiffanytoh2005@hotmail.com>
2017-11-14 23:16:48 +0800

    added start function

--- WhatsNearBy/whatsnearby.js
+++ WhatsNearBy/whatsnearby.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var client_ID = "AI0UWYY2U4G1QCQ50FRSGZRUZG0RIJUE0BLWBZWXCSHNTA2J";
-var client_secret ="4DPTTZWE2TQNVEX524RWJNHTIIAZXLJ5D4O52SSLGUNDKANQ";
+var client_secret = "4DPTTZWE2TQNVEX524RWJNHTIIAZXLJ5D4O52SSLGUNDKANQ";
 var radius = 1000;
 var longitude = '';
 var latitude = '';
@@ -9,19 +9,27 @@ var version = "20171114"
 
 var render = document.getElementById('view');
 
-function onchange_action(){
+function start() {
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(setCoordinate);
+    }
+
+}
+
+function onchange_action() {
     var e = document.getElementById('category_id');
     var section = e.options[e.selectedIndex].value;
     return section;
 }
 
-document.getElementById('category_id').onchange = function(){
+document.getElementById('category_id').onchange = function () {
     var section = onchange_action();
     latitude = initApp.navigator.geolocation.latitude;
     longitude = initApp.navigator.geolocation.longitude;
-    getEndPoint(latitude,longitude,section);
+    getEndPoint(latitude, longitude, section);
 }
 
 function getEndPoint(lat, long, section) {
-    return "https://api.foursquare.com/v2/venues/explore?ll="+lat+","+long+"&section=" + section +"&radius="+ radius+"&client_id="+ client_ID+"&client_secret="+client_secret+"&v=" + version;
+    return "https://api.foursquare.com/v2/venues/explore?ll=" + lat + "," + long + "&section=" + section + "&radius=" + radius + "&client_id=" + client_ID + "&client_secret=" + client_secret + "&v=" + version;
 }
