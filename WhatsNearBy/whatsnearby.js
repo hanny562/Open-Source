60c031be8eed1dd08fcae8019bf06f990d7650e0
hanny562 <hanny562@hotmail.com>
2017-11-14 22:47:22 +0800

    set up constant variable client_id, client_secret, radius, lat and long position and version; create function for drop downlist onchange; setup function getEndPoint;

---
+++ WhatsNearBy/whatsnearby.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var client_ID = "AI0UWYY2U4G1QCQ50FRSGZRUZG0RIJUE0BLWBZWXCSHNTA2J";
+var client_secret ="4DPTTZWE2TQNVEX524RWJNHTIIAZXLJ5D4O52SSLGUNDKANQ";
+var radius = 1000;
+var longitude = '';
+var latitude = '';
+var version = "20171114"
+
+function onchange_action(){
+    var e = document.getElementById('category_id');
+    var section = e.options[e.selectedIndex].value;
+    return section;
+}
+
+document.getElementById('category_id').onchange = function(){
+    var section = onchange_action();
+    latitude = initApp.navigator.geolocation.latitude;
+    longitude = initApp.navigator.geolocation.longitude;
+    getEndPoint(latitude,longitude,section);
+}
+
+function getEndPoint(lat, long, section) {
+    return "https://api.foursquare.com/v2/venues/explore?ll="+lat+","+long+"&section=" + section +"&radius="+ radius+"&client_id="+ client_ID+"&client_secret="+client_secret+"&v=" + version;
+}
